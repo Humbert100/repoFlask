568c93566cb8dc4740c2e38ab922510bd6efefbd
Humberto Alejandro Rosas Téllez <93540685+Humbert100@users.noreply.github.com>
2023-11-20 00:01:40 -0600

    Added more files

---
+++ app.py
@@ -0,0 +1,41 @@
+import io
+import os
+import json
+from PIL import Image
+
+import torch
+from flask import Flask, jsonify, url_for, render_template, request, redirect
+
+app = Flask(__name__)
+
+RESULT_FOLDER = os.path.join('static')
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+
+model = torch.hub.load('yolov5', 'custom', path='yolov5/runs/train/exp/weights/best.pt', source='local')
+model.eval()
+
+def get_prediction(img_bytes):
+    img = Image.open(io.BytesIO(img_bytes))
+    imgs = [img]  # batched list of images
+
+# Inference
+    results = model(imgs, size=640)  # includes NMS
+    return results
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return
+
+        img_bytes = file.read()
+        results = get_prediction(img_bytes)
+
+        results.save(save_dir='static')
+
+        full_filename = os.path.join(app.config['RESULT_FOLDER'], 'results0.jpg')
+        return redirect('static/image0.jpg')
+    return render_template('index.html')
